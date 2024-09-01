4d41e0dcf95d0362b890a31e1d3f2f0297962d43
tharanga_madurapperuma <tharanga.s.madurapperuma@gmail.com>
2024-09-01 22:43:35 +0530

    complete the setup

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,7 +4,7 @@ import React from 'react'
 const HomeScreen = () => {
   return (
     <View>
-      <Text className="text-red-500">HomeScreen</Text>
+      <Text className='text-red-500'>HomeScreen</Text>
     </View>
   )
 }
